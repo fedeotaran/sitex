d7594f2fe8b903382615a85a992c539bd5cab3cb
Fede Otaran <otaran.federico@gmail.com>
2020-04-25 13:53:28 -0300

    First commit, empty project

---
+++ lib/sitex.ex
@@ -0,0 +1,18 @@
+defmodule Sitex do
+  @moduledoc """
+  Documentation for `Sitex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sitex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sitex_test.exs
@@ -0,0 +1,8 @@
+defmodule SitexTest do
+  use ExUnit.Case
+  doctest Sitex
+
+  test "greets the world" do
+    assert Sitex.hello() == :world
+  end
+end
