592e70041c93ce5fe034931c37fb464f8ad2a106
Fede Otaran <otaran.federico@gmail.com>
2020-12-27 11:50:59 -0300

    Add build command

---
+++ lib/mix/tasks/build.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Sitex.Build do
+  use Mix.Task
+  require Logger
+
+  @shortdoc "Build de application."
+  def run(_) do
+    Logger.info("Generating files..")
+    Sitex.build()
+    Logger.info("All Done!")
+  end
+end
