8b4042ca5f2e4466adc5c5e9da23fe2b50bd1671
Fede Otaran <otaran.federico@gmail.com>
2022-05-14 11:38:14 -0300

    Add templates on watcher

--- lib/server/watcher.ex
+++ lib/server/watcher.ex
@@ -3,12 +3,17 @@ defmodule Sitex.Server.Watcher do
 
   require Logger
 
+  alias Sitex.Config
+
+  @content_path Map.get(Config.load(), :content, "content")
+  @templates_path Map.get(Config.load(), :templates, "templates")
+
   def start_link(args) do
     GenServer.start_link(__MODULE__, args)
   end
 
   def init(_args) do
-    {:ok, watcher_pid} = FileSystem.start_link(dirs: ["content"])
+    {:ok, watcher_pid} = FileSystem.start_link(dirs: [@content_path, @templates_path])
 
     FileSystem.subscribe(watcher_pid)
     {:ok, %{watcher_pid: watcher_pid}}
@@ -17,6 +22,7 @@ defmodule Sitex.Server.Watcher do
   def handle_info({:file_event, _pid, {_path, _e}}, %{watcher_pid: _watcher_pid} = state) do
     Logger.debug("rebuilding...")
     Sitex.Builder.build()
+    Logger.debug("done!")
     {:noreply, state}
   end
 
