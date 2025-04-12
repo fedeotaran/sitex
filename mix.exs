9650436eaeb7cbf10485f74056b24d63a06baad5
Fede Otaran <otaran.federico@gmail.com>
2025-04-12 23:09:21 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sitex.MixProject do
     [
       app: :sitex,
       version: "0.1.0",
-      elixir: "~> 1.10-rc",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,12 +22,12 @@ defmodule Sitex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"},
-      {:file_system, "~> 0.2"},
-      {:yamerl, "~> 0.8.0"},
+      {:plug_cowboy, "~> 2.6"},
+      {:file_system, "~> 0.2.10"},
+      {:yamerl, "~> 0.8.1"},
       {:nimble_publisher, path: "../nimble_publisher"},
-      {:earmark, "~> 1.4"},
-      {:makeup_elixir, "~> 0.16.0"},
+      {:earmark, "~> 1.4.37"},
+      {:makeup_elixir, "~> 0.16.1"},
       {:makeup_erlang, "~> 0.1.1"}
     ]
   end
