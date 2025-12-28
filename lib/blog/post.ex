a74563bfb04703caee07301732dcaf7ec85f61c0
Fede Otaran <otaran.federico@gmail.com>
2025-12-28 20:14:59 +0100

    Fix how to create the url for posts

--- lib/blog/post.ex
+++ lib/blog/post.ex
@@ -8,7 +8,7 @@ defmodule Sitex.Blog.Post do
     [year, month_day_id] = filename |> Path.rootname() |> Path.split() |> Enum.take(-2)
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
-    url = "blog/#{year}/#{month_day_id}"
+    url = "/blog/#{year}/#{month_day_id}"
 
     struct!(Post, [id: id, date: date, body: body, url: url] ++ Map.to_list(attrs))
   end
